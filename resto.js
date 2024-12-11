f8e2137aaa379640720cb0871afe990e9950f315
Celia506 <celia.sansano.dwwm48@gmail.com>
2024-12-11 15:04:37 +0100

    update

--- resto.js
+++ resto.js
@@ -2,3 +2,6 @@ function myFunction() {
     var element = document.body; 
     element.classList.toggle("dark-mode"); 
 }
+
+// document.getElementById("myForm").addEventListener("submit", function(event){ event.preventDefault();
+// var nom = document.getElementById
