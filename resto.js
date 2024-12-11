b4f3cb9bf1061a20c841cb223bf81ea7ebf1c803
Celia506 <celia.sansano.dwwm48@gmail.com>
2024-12-11 12:07:15 +0100

    update

--- resto.js
+++ resto.js
@@ -1,1 +1,5 @@
-resto.js
+const toggleTheme = document.getElementById('toggle-theme');
+const body = document.body;
+toggleTheme.addEventListener('click',() => {
+    body.classList.toggle('dark-mode');
+});
